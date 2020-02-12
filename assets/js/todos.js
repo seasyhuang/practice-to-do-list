bba0e5274fabb07a875ed27dca99f996953e63a4
Seasy <seasy.huang@gmail.com>
2020-02-12 11:21:34 -0500

    fdas

---
+++ assets/js/todos.js
@@ -0,0 +1,28 @@
+// check off specific todos by clicking
+// this is on ul so that NEW added li's can also be listened to
+// note the second argument "li" - we're listening to the whole ul, but function only runs when li's inside the ul are clicked
+$("ul").on("click", "li", function(){
+  $(this).toggleClass('completed');
+});
+
+// click on x to delete todo
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    // this refers to the first this parent (not the same this)
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+// create new todo
+$("input[type='text']").keypress(function(event){
+  // check if user clicked enter
+  if (event.which === 13) {
+      // extract value
+      var newtd = $("input").val();
+      // add to todolist as html
+      $("ul").append("<li><span>x</span> " + newtd + "</li>")
+      // clear input
+      $(this).val("");
+  }
+});
